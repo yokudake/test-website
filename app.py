656f548dfcd1b5641073252a92ee69452269da07
Kelly Y. <66449181+yokudake@users.noreply.github.com>
2024-04-16 20:15:30 +0000

    added job item
    added

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [
+  {
+    "id": 1,
+    "title": "Data Analyst",
+    "location": "Bengaluru, India",
+    "salary": "Rs. 10,00,000"
+  },
 
+  {
+    "id": 2,
+    "title": "Data Scientist",
+    "location": "Delhi, India",
+    "salary": "Rs. 15,00,000"
+  },
+
+  {
+    "id": 3,
+    "title": "Frontend Engineer",
+    "location": "Remote",
+    "salary": "Rs. 12,00,000"
+  },
+ 
+]
 @app.route("/")
 def hello():
-   return render_template('home.html')
+   return render_template('home.html', jobs=JOBS, companyname = "Kelly Co.")
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
    app.run(host= '0.0.0.0', debug = True)
